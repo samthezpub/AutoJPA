3c13f68f0cd70391ec4affcc0e2f001a22df98eb
Владимир <lil.rhymes9@gmail.com>
2023-10-03 01:41:37 +0300

    Error fixed

--- src/main/java/com/example/autojpa/Service/impl/RequestServiceImpl.java
+++ src/main/java/com/example/autojpa/Service/impl/RequestServiceImpl.java
@@ -1,9 +1,38 @@
 package com.example.autojpa.Service.impl;
 
+import com.example.autojpa.Entity.RequestEntity;
+import com.example.autojpa.Repository.RequestRepository;
 import com.example.autojpa.Service.RequestService;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RequestServiceImpl implements RequestService {
 
+    RequestRepository requestRepository;
+
+    public RequestServiceImpl(RequestRepository requestRepository) {
+        this.requestRepository = requestRepository;
+    }
+
+    @Override
+    public RequestEntity saveRequest(RequestEntity request) {
+        return requestRepository.save(request);
+    }
+
+    @Override
+    public Optional<RequestEntity> findById(Long id) {
+        return requestRepository.findById(id);
+    }
+
+    @Override
+    public RequestEntity updateRequest(RequestEntity request) {
+        return requestRepository.save(request);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        requestRepository.deleteById(id);
+    }
 }
