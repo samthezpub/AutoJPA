3a32f4efdfeaf59b9eac0994f2c269568ff683d9
Владимир <lil.rhymes9@gmail.com>
2023-10-02 23:09:09 +0300

    Initial commit

---
+++ src/main/java/com/example/autojpa/AutoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.autojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/autojpa/AutoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
